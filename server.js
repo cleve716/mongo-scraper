b5124f3298aae79f7a18c7b1c4b1e2a572667564
Cleve716 <cleve716@msn.com>
2019-07-26 16:57:25 -0600

    simplifying tree

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"); //dependencies
 var express = require("express");
-var exphbs = require("express-handlebars");
+var expressHbs = require("express-handlebars");
+var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000; //use port 3000
 
-var app = express();
+var app = express(); //express app
 
-var router = express.Router();
+var router = express.Router(); //A Router instance is a complete middleware and routing system
+require("./config/routes")(router);
 
 app.use(express.static(__dirname + "/public'"));
 
+app.engine("handlebars", expressHbs({ // npm syntax
+    defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ //will parse objects from JSON strings
+    extended: false
+}));
+
 app.use(router);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var mongdb = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(mongdb, function(error){
+if (error){
+    console.log(error);
+}
+else {
+    console.log("mongoose connected");
+}
+});
     
 app.listen(PORT, function() {
     console.log("Listening on port: " + PORT);
